cfbafd1c80a98a886db2baffe173d43925b5d3e9
mathias <mathias15052004@gmail.com>
2024-09-04 20:38:35 -0300

    Created the menu options

--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,47 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Menu m = new Menu();
-        m.showMenu();
+        m.showMenu(sc);
     }
 
-    public void showMenu() {
+    public void showMenu(Scanner sc) {
+        int op = 0;
         System.out.println("Bem vindo");
         System.out.println("Este programa é uma calculadora onde opera com dois parâmetros");
-        System.out.println("Digite a operação desejada");
+        do {
+            try {
+                System.out.println("Digite a operação desejada");
+                System.out.println("--------------------------");
+                System.out.println("-     1) Soma            -");
+                System.out.println("-     2) Subtração       -");
+                System.out.println("-     3) Multiplicação   -");
+                System.out.println("-     4) Divisão         -");
+                System.out.println("-     0) Sair            -");
+                System.out.println("--------------------------");
+                op = sc.nextInt();
+                switch(op) {
+                    case 0:
+                        System.exit(0);
+                    case 1:
+                        //soma
+                    case 2:
+                        //subtração
+                    case 3:
+                        //multiplicação
+                    case 4:
+                        //divisão
+                    default:
+                        System.out.println("Opção inválida");
+                }
+            } catch(InputMismatchException ime) {
+                System.err.println("Digite apenas números, tente novamente");
+                op = 0;
+            }
+        } while(op > 0);
     }
 }
