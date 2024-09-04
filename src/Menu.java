a28b6d5a65f61ce4b686774a66f72bbf4d30c9dc
mathias <mathias15052004@gmail.com>
2024-09-04 20:23:40 -0300

    Created menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,12 @@
 public class Menu {
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.showMenu();
+    }
 
+    public void showMenu() {
+        System.out.println("Bem vindo");
+        System.out.println("Este programa é uma calculadora onde opera com dois parâmetros");
+        System.out.println("Digite a operação desejada");
+    }
 }
