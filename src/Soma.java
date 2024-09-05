cf99e562bdcf93b00000f95d306033fbb1114b3f
mathias <mathias15052004@gmail.com>
2024-09-04 21:12:12 -0300

    Created menuSoma and escreverResultado methods

--- src/Soma.java
+++ src/Soma.java
@@ -1,6 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Soma {
     private double value1;
     private double value2;
+    private double result;
 
+    public Soma() {
 
+    }
+
+    public void menuSoma(Scanner sc) {
+        try {
+            System.out.println("Digite o primeiro número: ");
+            this.value1 = sc.nextDouble();
+            System.out.println("Digite o segundo número: ");
+            this.value2 = sc.nextDouble();
+            this.result = this.value1 + this.value2;
+        } catch (InputMismatchException ime) {
+            System.err.println("Digite apenas números");
+        }
+    }
+
+    public void escreverResultado() {
+        System.out.println("O resultado da soma " + value1 + " " + value2 + " é: " + result);
+    }
 }
